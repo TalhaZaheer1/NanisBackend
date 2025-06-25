4f6692c10bc703c4be73f1806450ae06cf48b8d3
TalhaZaheer1 <tzaheer72@gmail.com>
2025-06-25 21:26:08 +0500

    increased cookie max age

--- app.ts
+++ app.ts
@@ -2,19 +2,30 @@ import express from "express";
 import session from "express-session";
 import passport from "passport";
 import authRoutes from "./src/routes/auth";
-import userRoutes from "./src/routes/user"
+import userRoutes from "./src/routes/user";
 import "./src/config/passport";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 
-app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
-app.use(session({ secret: "secret123", resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: "secret123",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    },
+  }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/user",userRoutes)
+app.use("/api/user", userRoutes);
 
 export default app;
